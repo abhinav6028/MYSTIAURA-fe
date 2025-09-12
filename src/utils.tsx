335c510a3fbe7a8acb82366260726a3677518395
nishanth-ss <nishanthsekar1@gmail.com>
2025-09-12 20:26:59 +0530

    bestseller product api integrated done

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,2 +1,3 @@
 export const PRIMARY_COLOUR = "#660033"
 export const FONT_FAMILY = "Prata"
+export const finalPrice = (price: number, discountPrice: number) => price - (price * discountPrice) / 100;
