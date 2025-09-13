16e31f8ddaf8c20d0400edce10110f205d91634f
vipin760 <113933540+vipin760@users.noreply.github.com>
2025-09-13 10:21:12 +0530

    Update apiClient.ts

--- src/services/apiClient/apiClient.ts
+++ src/services/apiClient/apiClient.ts
@@ -1,44 +1,20 @@
 import axios from "axios";
 
-const apiClient = axios.create({
-  baseURL: "https://jwelery-be.onrender.com/",
-  headers: { "Content-Type": "application/json" },
-});
+const persistedRoot = localStorage.getItem("persist:root");
+const token =
+  persistedRoot &&
+  JSON.parse(persistedRoot).token &&
+  JSON.parse(JSON.parse(persistedRoot).token as string);
 
-// Attach token just before each request
-apiClient.interceptors.request.use((config) => {
-  const persistedRoot = localStorage.getItem("persist:root");
-  if (persistedRoot) {
-    const root = JSON.parse(persistedRoot);
-    const token = root.token && JSON.parse(root.token);
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-  }
-  return config;
+const apiClient = axios.create({
+  // baseURL: "http://localhost:5000/",
+  // baseURL: "https://1h48b83c-5000.inc1.devtunnels.ms/",
+  baseURL: "https://jwelery-be.onrender.com/",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
+
+  },
 });
 
 export default apiClient;
-
-
-
-// import axios from "axios";
-
-// const persistedRoot = localStorage.getItem("persist:root");
-// const token =
-//   persistedRoot &&
-//   JSON.parse(persistedRoot).token &&
-//   JSON.parse(JSON.parse(persistedRoot).token as string);
-
-// const apiClient = axios.create({
-//   // baseURL: "http://localhost:5000/",
-//   // baseURL: "https://1h48b83c-5000.inc1.devtunnels.ms/",
-//   baseURL: "https://jwelery-be.onrender.com/",
-//   headers: {
-//     "Content-Type": "application/json",
-//     Authorization: `Bearer ${token}`,
-
-//   },
-// });
-
-// export default apiClient;
