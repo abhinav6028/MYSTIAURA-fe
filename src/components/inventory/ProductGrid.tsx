642ddffaa20203bed1cf93fcca873ee97da1e538
abhinav6028 <abhinavgit8@gmail.com>
2025-09-28 12:31:05 +0530

    issue fixed when clicks an item item from the datail page gonna undefind

--- src/components/inventory/ProductGrid.tsx
+++ src/components/inventory/ProductGrid.tsx
@@ -1,23 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Heart } from "lucide-react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useProducts } from "../../services/api/product/product";
 import type { ProductCategory } from "../../types/categoryTypes";
+import { finalPrice } from "../../utils";
 
 const ProductGrid: React.FC = () => {
 
   const navigate = useNavigate();
-  const {categoryname} = useParams();
-  const {data : products} = useProducts(categoryname);
+  const { categoryname } = useParams();
+  const { data: products } = useProducts(categoryname);
   const productsData = products?.data?.products?.products || [];
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, []);
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 md:px-4 mb-5 md:mb-10">
-      { Array.isArray(productsData) && productsData?.map((val: ProductCategory, index: number) => (
+      {Array.isArray(productsData) && productsData?.map((val: ProductCategory, index: number) => (
         <div
           key={index}
           className="bg-[#f9f9f9] flex flex-col place-items relative cursor-pointer"
-          onClick={() => navigate('/productdetailPage')}
+          onClick={() => navigate(`/productdetailPage/${val?._id}`)}
         >
           <div className="w-full h-80 md:h-80 flex justify-center bg-[#f9f9f9]">
             <img
@@ -44,10 +49,21 @@ const ProductGrid: React.FC = () => {
               {val?.name}
             </h2>
 
-            <div className="flex items-baseline space-x-3">
-              <span className="text-lg font-semibold text-[#a37557]">INR {val?.price}</span>
-              {/* <span className="text-md text-gray-400 line-through">$250.00</span> */}
+            <div className="w-full bg-white">
+              <div className="flex items-baseline space-x-3">
+                <span className="text-lg font-semibold text-[#a37557]">
+                  ₹ {finalPrice(val.price, 10).toFixed(2)}
+                </span>
+                <span className="text-md text-gray-400 line-through">
+                  ₹ {val.price.toFixed(2)}
+                </span>
+              </div>
             </div>
+
+            {/* <div className="flex items-baseline space-x-3">
+              <span className="text-lg font-semibold text-[#a37557]">INR {val?.price}</span>
+              
+            </div> */}
           </div>
         </div>
       ))}
