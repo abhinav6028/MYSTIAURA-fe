88c6144c88a090282a1dd529899cce4f4c7bd7fb
abhinav6028 <abhinavgit8@gmail.com>
2025-09-28 12:20:24 +0530

    automatically scroll to top when the page is redirecting to detailpages

--- src/pages/ProductDetailPage/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage/ProductDetailPage.tsx
@@ -1,9 +1,14 @@
+import { useEffect } from 'react';
 import LayoutContainer from '../../components/layout/LayoutContainer'
 import ProductDetailsMain from '../../components/ProductDetailPage/ProductDetailsMain'
 import ReviewsAndProducts from '../../components/ProductDetailPage/ReviewsAndProducts'
 
 export default function ProductDetailPage() {
-    
+
+    useEffect(() => {
+        window.scrollTo({ top: 0, behavior: 'smooth' }); 
+    }, []);
+
     return (
         <LayoutContainer>
             <ProductDetailsMain />
